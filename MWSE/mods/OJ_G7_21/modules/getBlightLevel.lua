a294adc38edffaa4894d934a54c6676c3eed20f1
Greatness7 <Greatness7@gmail.com>
2021-04-02 23:38:03 -0400

    Remove some debug logs.

--- MWSE/mods/OJ_G7_21/modules/getBlightLevel.lua
+++ MWSE/mods/OJ_G7_21/modules/getBlightLevel.lua
@@ -6,7 +6,6 @@ local function getBlightedCells()
             table.insert(blightedCells, {cell.gridX, cell.gridY})
         end
     end
-    mwse.log(json.encode(blightedCells, {indent=true}))
 end
 event.register("initialized", getBlightedCells)
 
@@ -20,8 +19,6 @@ end
 -- Levels range 0 to 5. With 5 being the most-blighted areas.
 -- The levels are calculated from the distance to the closest blighted region.
 local function getBlightLevel(cell)
-    local grid = {cell.gridX, cell.gridY}
-
     if cell.region == nil then
         return 0
     elseif cell.region.weatherChanceBlight > 0 then
@@ -29,15 +26,14 @@ local function getBlightLevel(cell)
     end
 
     local min_dist = 5
-    for i, g in ipairs(blightedCells) do
-        local dist = math.floor(distance2d(grid, g))
-        debug.log(dist)
+
+    local current_grid = {cell.gridX, cell.gridY}
+    for _, blight_grid in ipairs(blightedCells) do
+        local dist = math.floor(distance2d(current_grid, blight_grid))
         if dist < min_dist then
             min_dist = dist
         end
     end
-
-    mwse.log(min_dist)
 
     return 5 - min_dist
 end
