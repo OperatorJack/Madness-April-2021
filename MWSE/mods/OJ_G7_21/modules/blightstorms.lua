7520ec082512a5313f327be26609f8ec18e634e9
OperatorJack <thepaladinof@gmail.com>
2021-04-02 22:33:43 -0400

    Implemented service refusal while in blight storms.

--- MWSE/mods/OJ_G7_21/modules/blightstorms.lua
+++ MWSE/mods/OJ_G7_21/modules/blightstorms.lua
@@ -4,12 +4,16 @@ local common = require("OJ_G7_21.common")
 
 event.register("cellChanged", function(e)
     if (e.cell.isInterior == true and e.cell.behavesAsExterior ~= true) then
+        tes3.setGlobal("TB_IsInExternalCell", 0)
         return
     end
 
     if (tes3.worldController.weatherController.currentWeather.index ~= tes3.weather.blight) then
         return
     end
+
+    -- Update Cell Position global.
+    tes3.setGlobal("TB_IsInExternalCell", 1)
 
     -- Only proc if chance is met.
     if (math.random(0, 100) > common.calculateBlightChance(tes3.player)) then
