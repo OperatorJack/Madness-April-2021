2cd93617242e0c596830bb424e0aa7d553f280df
Greatness7 <Greatness7@gmail.com>
2021-04-03 20:14:33 -0400

    refactor decal mapping stuff

--- MWSE/mods/blight/modules/decal-mapping.lua
+++ MWSE/mods/blight/modules/decal-mapping.lua
@@ -1,6 +1,25 @@
 local common = require("blight.common")
 
-local function traverseNIF(roots)
+local bodyPartBlacklist = {
+    [tes3.activeBodyPart.groin] = true,
+    [tes3.activeBodyPart.hair] = true,
+    [tes3.activeBodyPart.leftPauldron] = true,
+    [tes3.activeBodyPart.rightPauldron] = true,
+    [tes3.activeBodyPart.shield] = true,
+    [tes3.activeBodyPart.weapon] = true,
+}
+
+local decalTextures = {
+    ["textures\\blight\\decal_blight1.dds"] = true,
+    ["textures\\blight\\decal_blight2.dds"] = true,
+}
+event.register("initialized", function()
+    for k, v in pairs(decalTextures) do
+        decalTextures[k] = niSourceTexture.createFromPath(k)
+    end
+end)
+
+local function traverse(roots)
     local function iter(nodes)
         for i, node in ipairs(nodes or roots) do
             if node then
@@ -14,104 +33,63 @@ local function traverseNIF(roots)
     return coroutine.wrap(iter)
 end
 
-local texture
-local function onInitialized()
-    texture = niSourceTexture.createFromPath("textures\\blight\\decal_blight1.dds")
-end
-event.register("initialized", onInitialized)
-
-
-local managedReferences = {}
-
-local function onObjectInvalidated(e)
-    managedReferences[e.object] = nil
-end
-event.register("objectInvalidated", onObjectInvalidated)
-
-local function addBlightDecal(e)
-    local reference = e.reference
-
-    if reference.object.organic ~= true then
-        reference:updateEquipment()
-        return
-    end
-
-    for node in traverseNIF({ reference.sceneNode }) do
-        local success, texturingProperty, alphaProperty = pcall(function() return node:getProperty(0x4), node:getProperty(0x0) end)
-        if (success and texturingProperty and not alphaProperty) then
-            if (texturingProperty.canAddDecal) then
+local function addBlightDecal(sceneNode)
+    for node in traverse{sceneNode} do
+        local success, texturingProperty, alphaProperty = pcall(function()
+            return node:getProperty(0x4), node:getProperty(0x0)
+        end)
+        if success and texturingProperty and not alphaProperty then
+            if texturingProperty.canAddDecal then
+                local texture = table.choice(decalTextures)
                 local map, index = texturingProperty:addDecalMap(texture)
-                if (map) then
-                    common.debug("Added decal to '%s' (%s) at index %d", node.name, node.RTTI.name, index)
-                    managedReferences[reference] = true 
+                if map then
+                    common.debug("Added decal to '%s' at index %d", node.name, index)
                 end
             end
         end
     end
 end
 
-local function removeBlightDecal(e)
-    local reference = e.reference
-
-    -- TODO
+local function removeBlightDecal(sceneNode)
+    for node in traverse{sceneNode} do
+        pcall(function()
+            local texturingProperty = node:getProperty(0x4)
+            for i, map in ipairs(texturingProperty.maps) do
+                if decalTextures[map.texture.fileName] then
+                    texturingProperty:removeDecalMap(i)
+                end
+            end
+        end)
+    end
 end
 
-local blacklist = {
-    [tes3.activeBodyPart.groin] = true,
-    [tes3.activeBodyPart.weapon] = true,
-    [tes3.activeBodyPart.shield] = true,
-    [tes3.activeBodyPart.leftPauldron] = true,
-    [tes3.activeBodyPart.rightPauldron] = true,
-    [tes3.activeBodyPart.hair] = true,
-}
 event.register("bodyPartAssigned", function(e)
-    -- bail if an object assigned to this bodypart
+    -- ignore covered slots
     if e.object ~= nil then return end
 
-    -- ignore hair, groin, etc.
-    if blacklist[e.index] then return end
+    -- ignore blacklisted slots
+    if bodyPartBlacklist[e.index] then return end
 
+    -- ignore non-blighted NPCs
     if not common.hasBlight(e.reference) then return end
 
+    -- the bodypart scene node is available on the next frame
     timer.delayOneFrame(function()
-        local sceneNode = e.manager:getActiveBodyPartNode(tes3.activeBodyPartLayer.base, e.index);
-        if not sceneNode then return end
-
-        for node in traverseNIF({ sceneNode }) do
-            local success, texturingProperty, alphaProperty = pcall(function() return node:getProperty(0x4), node:getProperty(0x0) end)
-            if (success and texturingProperty and not alphaProperty) then
-                    local map, index = texturingProperty:addDecalMap(texture)
-                if (map) then
-                    common.debug("Added decal to '%s' (%s) at index %d", node.name, node.RTTI.name, index)
-                    managedReferences[e.reference] = true
-                end
-            end
-        end
-
+        local sceneNode = e.manager:getActiveBodyPartNode(tes3.activeBodyPartLayer.base, e.index)
+        addBlightDecal(sceneNode)
     end)
 end)
 
 event.register("referenceActivated", function(e)
-    local object = e.reference.object
-
-    -- ensure the reference is a cont
-    if  object.organic ~= true 
-        and object.objectType ~= tes3.objectType.npc
-        and object.objectType ~= tes3.objectType.creature then
-        return
-    end
-
-    -- if actor has blight, decal map em.
-    if common.hasBlight(e.reference) then
-        addBlightDecal(e)
+    if e.reference.object.organic then
+        if common.hasBlight(e.reference) then
+            addBlightDecal(e.reference.sceneNode)
+        end
     end
 end)
 
-
 event.register("loaded", function(e)
-    if common.hasBlight(tes3.player) then
-        tes3.player:updateEquipment()
-    end
+    tes3.player:updateEquipment()
 end)
 
 event.register("blight:AddedBlight", addBlightDecal)
