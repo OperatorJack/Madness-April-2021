5a352eeb322e0741c6863f5d94dc688163d14667
Greatness7 <Greatness7@gmail.com>
2021-04-03 22:10:55 -0400

    Restrict passive transmission to once per day

--- MWSE/mods/blight/modules/passive-transmission.lua
+++ MWSE/mods/blight/modules/passive-transmission.lua
@@ -17,18 +17,29 @@ local function onRefCreated(e)
         return
     end
 
+    -- get reference blight data
+    local data = e.reference.data
+    data.blight = data.blight or {}
+
+    -- restrict passive transmission to once per day
+    local daysPassed = tes3.worldController.daysPassed.value
+    if data.blight.lastEncounter ~= daysPassed then
+        data.blight.lastEncounter = daysPassed
+    else
+        common.debug("%s already encountered today (passive transmission disabled)", e.reference)
+        return
+    end
+
     -- Check for expired blight diseases on reference, remove if expired.
-    if  e.reference.data.blight and
-        e.reference.data.blight.passiveTransmission then
-        for spellId, day in pairs(e.reference.data.blight.passiveTransmission) do
-            if day <= tes3.worldController.daysPassed.value then
+    if data.blight.passiveTransmission then
+        for spellId, day in pairs(data.blight.passiveTransmission) do
+            if day <= daysPassed then
                 common.debug("%s recovered from %s blight disease!", e.reference, spellId)
                 common.removeBlight(e.reference, spellId)
-                e.reference.data.blight.passiveTransmission[spellId] = nil
+                data.blight.passiveTransmission[spellId] = nil
             end
         end
     end
- 
 
     -- roll for chance of catching blight disease
     -- blight level 1 -> 1*4 == 4%
@@ -39,14 +50,14 @@ local function onRefCreated(e)
 
     if threshold > roll then
         common.debug("%s has caught blight disease! (threshold=%s vs roll=%s)", e.reference, threshold, roll)
-        event.trigger("blight:TriggerDisease", { 
-            reference = e.reference, 
-            displayMessage = false, 
+        event.trigger("blight:TriggerDisease", {
+            reference = e.reference,
+            displayMessage = false,
             callback = function (spell)
                 -- Setup information to remove disease later.
-                e.reference.data.blight = e.reference.data.blight or {}
-                e.reference.data.blight.passiveTransmission = e.reference.data.blight.passiveTransmission or {}
-                e.reference.data.blight.passiveTransmission[spell.id] = tes3.worldController.daysPassed.value + math.random(2,8)
+                data.blight = data.blight or {}
+                data.blight.passiveTransmission = data.blight.passiveTransmission or {}
+                data.blight.passiveTransmission[spell.id] = daysPassed + math.random(2,8)
             end
         })
     end
