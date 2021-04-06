50e48f1ad579d206e178436fb2dd1679d4740552
OperatorJack <thepaladinof@gmail.com>
2021-04-05 20:11:34 -0400

    Implement two pass calculation for passive transmission. Check for mechanic first, then for normal blight transmission chance.

--- MWSE/mods/blight/modules/passive-transmission.lua
+++ MWSE/mods/blight/modules/passive-transmission.lua
@@ -41,25 +41,34 @@ local function onRefCreated(e)
         end
     end
 
-    -- roll for chance of catching blight disease
-    -- blight level 1 -> 1*4 == 4%
-    -- blight level 3 -> 3*4 == 12%
-    -- blight level 5 -> 5*4 == 20%
-    local threshold = blightLevel * 4
-    local roll = math.random(100)
-
-    if threshold > roll then
-        common.debug("%s has caught blight disease! (threshold=%s vs roll=%s)", e.reference, threshold, roll)
-        event.trigger("blight:TriggerDisease", {
-            reference = e.reference,
-            displayMessage = false,
-            callback = function (spell)
-                -- Setup information to remove disease later.
-                data.blight = data.blight or {}
-                data.blight.passiveTransmission = data.blight.passiveTransmission or {}
-                data.blight.passiveTransmission[spell.id] = daysPassed + math.random(2,8)
-            end
-        })
+    -- roll for chance of triggering passive transmission mechanic.
+    -- blight level 1 -> 1*5 == 5%
+    -- blight level 3 -> 3*5 == 15%
+    -- blight level 5 -> 5*5 == 25%
+    local chance = blightLevel * 5
+    if common.calculateChanceResult(chance) == false then
+        -- Reference does not trigger mechanic.
+        return
     end
+
+    -- roll for chance of actually getting blight.
+    chance = common.calculateBlightChance(e.reference)
+    if common.calculateChanceResult(chance) == false then
+        -- Reference avoids catching blight.
+        return
+    end
+
+
+    common.debug("%s has caught blight disease from passive transmission!", e.reference)
+    event.trigger("blight:TriggerDisease", {
+        reference = e.reference,
+        displayMessage = false,
+        callback = function (spell)
+            -- Setup information to remove disease later.
+            data.blight = data.blight or {}
+            data.blight.passiveTransmission = data.blight.passiveTransmission or {}
+            data.blight.passiveTransmission[spell.id] = daysPassed + math.random(2,4)
+        end
+    })
 end
 event.register("referenceActivated", onRefCreated)
